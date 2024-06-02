56c01f35b2dd64c53777f8912a6b0db0ff9a168f
Sriram Karthik Akella <sriramka@ualberta.ca>
2024-06-02 07:31:56 +0530

    Update 2June2024 - 344. Reverse String.py

--- 2June2024 - 344. Reverse String.py
+++ 2June2024 - 344. Reverse String.py
@@ -3,4 +3,7 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
-        
+        left, right = 0, len(s) - 1
+        while left < right:
+            s[left], s[right] = s[right], s[left]
+            left, right = left + 1, right - 1
