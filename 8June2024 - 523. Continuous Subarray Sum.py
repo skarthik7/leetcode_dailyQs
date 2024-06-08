ff52cd40450e261c66e4f604db6d360a38368512
Sriram Karthik Akella <sriramka@ualberta.ca>
2024-06-08 14:50:02 +0300

    Update 8June2024 - 523. Continuous Subarray Sum.py

---
+++ 8June2024 - 523. Continuous Subarray Sum.py
@@ -0,0 +1,13 @@
+class Solution:
+    def checkSubarraySum(self, nums: List[int], k: int) -> bool:
+        remainder_map = {0: -1}
+        cum_sum = 0
+        for i, num in enumerate(nums):
+            cum_sum += num
+            remainder = cum_sum % k
+            if remainder in remainder_map:
+                if i - remainder_map[remainder] > 1:
+                    return True
+            else:
+                remainder_map[remainder] = i
+        return False
