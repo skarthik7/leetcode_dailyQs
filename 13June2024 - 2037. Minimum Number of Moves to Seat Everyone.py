443d5d943b73c39af09c405aefae8a739453658c
Sriram Karthik Akella <sriramka@ualberta.ca>
2024-06-13 21:11:17 +0300

    Update 13June2024 - 2037. Minimum Number of Moves to Seat Everyone.py

--- 13June2024 - 2037. Minimum Number of Moves to Seat Everyone.py
+++ 13June2024 - 2037. Minimum Number of Moves to Seat Everyone.py
@@ -1,3 +1,5 @@
 class Solution:
     def minMovesToSeat(self, seats: List[int], students: List[int]) -> int:
-        pass
+        seats.sort()
+        students.sort()
+        return sum(abs(seat - student) for seat, student in zip(seats, students))
