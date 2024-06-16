4c57abad4124da8ba61f56feb9b6daf5ff878d7e
Sriram Karthik Akella <sriramka@ualberta.ca>
2024-06-16 23:45:15 +0300

    Update 16June2024 - 330. Patching Array.py

--- 16June2024 - 330. Patching Array.py
+++ 16June2024 - 330. Patching Array.py
@@ -1,4 +1,19 @@
 class Solution:
     def minPatches(self, nums: List[int], n: int) -> int:
-        pass
-        #TO DO
+        miss = 1
+        patches = 0
+        index = 0
+        
+        while miss <= n:
+            if index < len(nums) and nums[index] <= miss:
+                # If the current number is within the range we can form
+                # Then update miss
+                miss += nums[index]
+                index += 1
+            else:
+                # If the current number is outside the range
+                # Then add miss to the array (conceptually)
+                miss *= 2
+                patches += 1
+        
+        return patches
