302a640468a38ac02dcc66796a7d11b0c550f575
Sriram Karthik Akella <sriramka@ualberta.ca>
2024-06-18 13:18:29 +0300

    Update 18June2024 - 826. Most Profit Assigning Work.py

--- 18June2024 - 826. Most Profit Assigning Work.py
+++ 18June2024 - 826. Most Profit Assigning Work.py
@@ -1,3 +1,15 @@
 class Solution:
-    def maxProfitAssignment(self, difficulty: List[int], profit: List[int], worker: List[int]) -> int:
+    def maxProfitAssignment(self, difficulty, profit, worker):
+        jobs = sorted(zip(difficulty, profit))
+        worker.sort()
+        max_profit = 0
+        total_profit = 0
+        j = 0
         
+        for ability in worker:
+            while j < len(jobs) and ability >= jobs[j][0]:
+                max_profit = max(max_profit, jobs[j][1])
+                j += 1
+            total_profit += max_profit
+        
+        return total_profit
