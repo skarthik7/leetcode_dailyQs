8ca9bc603018dbd51a7119ba058f373b2dc2533a
Sriram Karthik Akella <sriramka@ualberta.ca>
2024-06-24 10:50:53 +0300

    Update 24June2024 - 995. Minimum Number of K Consecutive Bit Flips.py

--- 24June2024 - 995. Minimum Number of K Consecutive Bit Flips.py
+++ 24June2024 - 995. Minimum Number of K Consecutive Bit Flips.py
@@ -1,3 +1,14 @@
+from collections import deque
 class Solution:
     def minKBitFlips(self, nums: List[int], k: int) -> int:
-        pass
+        flips = 0
+        flipQueue = deque()
+        for i in range(len(nums)):
+            if flipQueue and flipQueue[0] == i:
+                flipQueue.popleft()
+            if len(flipQueue) % 2 == nums[i]:
+                if i + k > len(nums):
+                    return -1
+                flips += 1
+                flipQueue.append(i + k)
+        return flips
