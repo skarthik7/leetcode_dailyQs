97b62750a13a835e0312d5fe0816adf978529dc8
Sriram Karthik Akella <sriramka@ualberta.ca>
2024-06-27 21:02:43 +0530

    Update 27June2024 - 1791. Find Center of Star Graph.py

--- 27June2024 - 1791. Find Center of Star Graph.py
+++ 27June2024 - 1791. Find Center of Star Graph.py
@@ -1,3 +1,6 @@
 class Solution:
     def findCenter(self, edges: List[List[int]]) -> int:
-        pass
+        if edges[0][0] in edges[1]:
+            return edges[0][0]
+        else:
+            return edges[0][1]
