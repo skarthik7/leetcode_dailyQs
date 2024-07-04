aaaf73b4cb0854db4d74ed3952a7ca2d1f6b62bc
Sriram Karthik Akella <sriramka@ualberta.ca>
2024-07-04 21:49:01 +0530

    Update 4July2024 - 2181. Merge Nodes in Between Zeros.py

--- 4July2024 - 2181. Merge Nodes in Between Zeros.py
+++ 4July2024 - 2181. Merge Nodes in Between Zeros.py
@@ -5,4 +5,19 @@
 #         self.next = next
 class Solution:
     def mergeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        pass
+        dummy = ListNode(0)
+        current = dummy
+        node = head.next 
+        segment_sum = 0 
+        
+        while node:
+            if node.val != 0:
+                segment_sum += node.val
+            else:
+                if segment_sum != 0:
+                    current.next = ListNode(segment_sum)
+                    current = current.next
+                    segment_sum = 0
+            node = node.next
+        
+        return dummy.next
