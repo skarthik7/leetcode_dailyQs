b32ff2fb657fc73d2a4ef5f4e07cf4115c4e9110
Sriram Karthik Akella <sriramka@ualberta.ca>
2024-07-11 21:17:13 +0530

    Update 11July2024 - 1190. Reverse Substrings Between Each Pair of Parentheses.py

--- 11July2024 - 1190. Reverse Substrings Between Each Pair of Parentheses.py
+++ 11July2024 - 1190. Reverse Substrings Between Each Pair of Parentheses.py
@@ -1,3 +1,14 @@
 class Solution:
     def reverseParentheses(self, s: str) -> str:
-        
+        stack = []
+        for char in s:
+            if char == ')':
+                temp = ''
+                while stack[-1] != '(':
+                    temp += stack.pop()
+                stack.pop() 
+                for item in temp:
+                    stack.append(item)
+            else:
+                stack.append(char)
+        return ''.join(stack)
