4e3b67fd2c7f6bb9944f03e51abc82d52bbcb862
Sriram Karthik Akella <sriramka@ualberta.ca>
2024-07-12 19:41:19 +0530

    Update 12July2024 - 1717. Maximum Score From Removing Substrings.py

--- 12July2024 - 1717. Maximum Score From Removing Substrings.py
+++ 12July2024 - 1717. Maximum Score From Removing Substrings.py
@@ -1,3 +1,23 @@
 class Solution:
     def maximumGain(self, s: str, x: int, y: int) -> int:
+
+        if x > y:
+            first, second, first_points, second_points = "ab", "ba", x, y
+        else:
+            first, second, first_points, second_points = "ba", "ab", y, x
         
+        def remove_substring_and_calculate_points(s: str, sub: str, points: int) -> (str, int):
+            stack = []
+            score = 0
+            for char in s:
+                if stack and stack[-1] + char == sub:
+                    stack.pop()
+                    score += points
+                else:
+                    stack.append(char)
+            return ''.join(stack), score
+        
+        modified_s, score = remove_substring_and_calculate_points(s, first, first_points)
+        _, additional_score = remove_substring_and_calculate_points(modified_s, second, second_points)
+        
+        return score + additional_score
