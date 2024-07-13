27845aedd7cfe270c7183fe577d54aa272b346f6
Sriram Karthik Akella <sriramka@ualberta.ca>
2024-07-13 14:43:12 +0530

    Update 13July2024 - 2751. Robot Collisions.py

--- 13July2024 - 2751. Robot Collisions.py
+++ 13July2024 - 2751. Robot Collisions.py
@@ -1,3 +1,32 @@
 class Solution:
-    def survivedRobotsHealths(self, positions: List[int], healths: List[int], directions: str) -> List[int]:
+    def survivedRobotsHealths(positions, healths, directions):
+        robots = sorted([(positions[i], healths[i], directions[i]) for i in range(len(positions))])
+        
+        right_moving = []
+        survivors = [] 
+        
+        for pos, health, direction in robots:
+            if direction == 'L':
+                while right_moving and health > 0:
+                    _, r_health = right_moving[-1]
+                    if health > r_health:
+                        health -= 1
+                        right_moving.pop()
+                    elif health < r_health:
+                        right_moving[-1][1] -= 1
+                        health = 0
+                    else:
+                        right_moving.pop()
+                        health = 0
+                if health > 0:
+                    survivors.append((pos, health, direction))
+            else:
+                right_moving.append([pos, health])
+        
+        for pos, health in right_moving:
+            survivors.append((pos, health, 'R'))
+        
+        survivors.sort(key=lambda x: positions.index(x[0]))
+        
+        return [health for _, health, _ in survivors]
         
