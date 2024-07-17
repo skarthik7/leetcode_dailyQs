81440c205399bd0fae57697bc0c895b3811447fd
Sriram Karthik Akella <sriramka@ualberta.ca>
2024-07-17 22:28:46 +0530

    Update 17July2024 - 1110. Delete Nodes And Return Forest.py

--- 17July2024 - 1110. Delete Nodes And Return Forest.py
+++ 17July2024 - 1110. Delete Nodes And Return Forest.py
@@ -6,4 +6,18 @@
 #         self.right = right
 class Solution:
     def delNodes(self, root: Optional[TreeNode], to_delete: List[int]) -> List[TreeNode]:
-        
+        to_delete_set = set(to_delete)
+        forest = []
+
+        def dfs(node, is_root):
+            if not node:
+                return None
+            node_deleted = node.val in to_delete_set
+            if is_root and not node_deleted:
+                forest.append(node)
+            node.left = dfs(node.left, node_deleted)
+            node.right = dfs(node.right, node_deleted)
+            return None if node_deleted else node
+
+        dfs(root, True)
+        return forest
