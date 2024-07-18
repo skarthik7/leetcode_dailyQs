d1be2a23cad9a99b413506a81d108219bd5dca4e
Sriram Karthik Akella <sriramka@ualberta.ca>
2024-07-18 18:21:14 +0530

    Update 18July2024 - 1530. Number of Good Leaf Nodes Pairs.py

--- 18July2024 - 1530. Number of Good Leaf Nodes Pairs.py
+++ 18July2024 - 1530. Number of Good Leaf Nodes Pairs.py
@@ -7,4 +7,30 @@
 #         self.right = right
 class Solution:
     def countPairs(self, root: TreeNode, distance: int) -> int:
-        pass
+        self.good_pairs = 0
+
+        def dfs(node):
+            if not node:
+                return [0] * (distance + 1)
+            if not node.left and not node.right:
+                leaf_depths = [0] * (distance + 1)
+                leaf_depths[1] = 1
+                return leaf_depths
+            
+            left_depths = dfs(node.left)
+            right_depths = dfs(node.right)
+            
+            # Count good pairs
+            for d1 in range(1, distance):
+                for d2 in range(1, distance - d1 + 1):
+                    self.good_pairs += left_depths[d1] * right_depths[d2]
+            
+            # Update depths for the current node
+            new_depths = [0] * (distance + 1)
+            for d in range(1, distance):
+                new_depths[d + 1] = left_depths[d] + right_depths[d]
+            
+            return new_depths
+        
+        dfs(root)
+        return self.good_pairs
