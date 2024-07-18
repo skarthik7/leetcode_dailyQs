a6a536733bcefd843794598696452ecc30b948a8
Sriram Karthik Akella <sriramka@ualberta.ca>
2024-07-18 18:20:09 +0530

    Update 18July2024 - 1530. Number of Good Leaf Nodes Pairs.py

--- 18July2024 - 1530. Number of Good Leaf Nodes Pairs.py
+++ 18July2024 - 1530. Number of Good Leaf Nodes Pairs.py
@@ -1,1 +1,10 @@
 # 1530. Number of Good Leaf Nodes Pairs
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def countPairs(self, root: TreeNode, distance: int) -> int:
+        pass
