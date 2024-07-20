fed5314920fafc8cb938363016b3331515376bfc
Sriram Karthik Akella <sriramka@ualberta.ca>
2024-07-20 21:07:33 +0530

    Update 20July2024 - 1380. Lucky Numbers in a Matrix.py

--- 20July2024 - 1380. Lucky Numbers in a Matrix.py
+++ 20July2024 - 1380. Lucky Numbers in a Matrix.py
@@ -1,3 +1,14 @@
 class Solution:
     def luckyNumbers (self, matrix: List[List[int]]) -> List[int]:
-        pass
+        col_max = [0]*len(matrix[0])
+        row_min = [float('inf')]*len(matrix)
+        for i in range (len(matrix)):
+            row_min[i] = min(matrix[i])
+            for j in range(len(matrix[i])):
+                col_max[j] = max(col_max[j],matrix[i][j])
+        row_min_set = set(row_min)
+        col_max_set = set(col_max)
+
+
+        lucky_numbers = row_min_set.intersection(col_max_set)
+        return lucky_numbers
