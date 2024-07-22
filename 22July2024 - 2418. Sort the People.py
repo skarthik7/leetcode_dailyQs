b51a413656e46f366f30493dafa7b233cd79762b
Sriram Karthik Akella <sriramka@ualberta.ca>
2024-07-22 23:51:08 +0530

    Update 22July2024 - 2418. Sort the People.py

--- 22July2024 - 2418. Sort the People.py
+++ 22July2024 - 2418. Sort the People.py
@@ -6,3 +6,4 @@ class Solution:
         sorted_pairs = sorted(paired, key=lambda x: x[1], reverse=True)
         sorted_names = [name for name, height in sorted_pairs]
         return sorted_names
+# Time Complexity: O(NlogN) where N is the number of people.
