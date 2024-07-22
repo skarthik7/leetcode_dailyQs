04e410d2adba234081fdd3d7679c216b398ef26f
Sriram Karthik Akella <sriramka@ualberta.ca>
2024-07-22 23:50:57 +0530

    Update 22July2024 - 2418. Sort the People.py

--- 22July2024 - 2418. Sort the People.py
+++ 22July2024 - 2418. Sort the People.py
@@ -1,3 +1,8 @@
+from typing import List
+
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        pass
+        paired = zip(names, heights)
+        sorted_pairs = sorted(paired, key=lambda x: x[1], reverse=True)
+        sorted_names = [name for name, height in sorted_pairs]
+        return sorted_names
