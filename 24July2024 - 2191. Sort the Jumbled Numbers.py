9aca5d56b0531a3919387409487e8dc5e26dc9ff
Sriram Karthik Akella <sriramka@ualberta.ca>
2024-07-25 23:25:32 +0530

    Update 24July2024 - 2191. Sort the Jumbled Numbers.py

--- 24July2024 - 2191. Sort the Jumbled Numbers.py
+++ 24July2024 - 2191. Sort the Jumbled Numbers.py
@@ -1,3 +1,14 @@
+from typing import List
+
 class Solution:
     def sortJumbled(self, mapping: List[int], nums: List[int]) -> List[int]:
-        pass
+        def map_number(num: int) -> int:
+            mapped_str = ''.join(str(mapping[int(digit)]) for digit in str(num))
+            return int(mapped_str)
+        
+        mapped_nums = [(map_number(num), num) for num in nums]
+        
+        mapped_nums.sort(key=lambda x: x[0])
+        
+        sorted_nums = [num for _, num in mapped_nums]
+        return sorted_nums
