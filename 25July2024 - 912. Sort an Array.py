11315ef2cc0b8003a331993c1fd67ffc8fe62dd4
Sriram Karthik Akella <sriramka@ualberta.ca>
2024-07-25 23:12:03 +0530

    Update 25July2024 - 912. Sort an Array.py

---
+++ 25July2024 - 912. Sort an Array.py
@@ -0,0 +1,30 @@
+from typing import List
+
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        if len(nums) <= 1:
+            return nums
+        
+        def merge(left: List[int], right: List[int]) -> List[int]:
+            sorted_array = []
+            i = j = 0
+            
+            while i < len(left) and j < len(right):
+                if left[i] < right[j]:
+                    sorted_array.append(left[i])
+                    i += 1
+                else:
+                    sorted_array.append(right[j])
+                    j += 1
+            
+            sorted_array.extend(left[i:])
+            sorted_array.extend(right[j:])
+            
+            return sorted_array
+        
+        mid = len(nums) // 2
+        left_half = self.sortArray(nums[:mid])
+        right_half = self.sortArray(nums[mid:])
+        
+        return merge(left_half, right_half)
+        
