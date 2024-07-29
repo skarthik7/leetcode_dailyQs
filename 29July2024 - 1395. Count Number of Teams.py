a83815474d282a3af8ce6fc480617db6b7d37469
Sriram Karthik Akella <sriramka@ualberta.ca>
2024-07-29 22:36:42 +0300

    Update 29July2024 - 1395. Count Number of Teams.py

--- 29July2024 - 1395. Count Number of Teams.py
+++ 29July2024 - 1395. Count Number of Teams.py
@@ -14,3 +14,28 @@ class Solution:
 
 # Time complexity: O(n^3)
 
+class Solution:
+    def numTeams(self, rating: List[int]) -> int:
+        n = len(rating)
+        count = 0
+        
+        for j in range(n):
+            left_smaller = left_larger = right_smaller = right_larger = 0
+            
+            for i in range(j):
+                if rating[i] < rating[j]:
+                    left_smaller += 1
+                elif rating[i] > rating[j]:
+                    left_larger += 1
+            
+            for k in range(j + 1, n):
+                if rating[k] < rating[j]:
+                    right_smaller += 1
+                elif rating[k] > rating[j]:
+                    right_larger += 1
+            
+            count += left_smaller * right_larger + left_larger * right_smaller
+        
+        return count
+    
+# Time Complexity: (O(n^2)) because we use two nested loops to count the smaller and larger elements for each soldier.
