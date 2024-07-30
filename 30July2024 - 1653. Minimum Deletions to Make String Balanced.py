be391c14ed3c79729415f2bb9116a9a6b6a3e602
Sriram Karthik Akella <sriramka@ualberta.ca>
2024-07-30 08:24:03 +0300

    Update 30July2024 - 1653. Minimum Deletions to Make String Balanced.py

--- 30July2024 - 1653. Minimum Deletions to Make String Balanced.py
+++ 30July2024 - 1653. Minimum Deletions to Make String Balanced.py
@@ -1,3 +1,16 @@
 class Solution:
     def minimumDeletions(self, s: str) -> int:
+        count_a = s.count('a')
+        min_deletions = float('inf')
+        count_b = 0
         
+        for char in s:
+            if char == 'a':
+                count_a -= 1
+            else:
+                count_b += 1
+            
+            min_deletions = min(min_deletions, count_a + count_b)
+        
+        return min_deletions
+
