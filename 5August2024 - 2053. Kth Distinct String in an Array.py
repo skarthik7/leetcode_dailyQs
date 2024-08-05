4b82d311b3be9181951b406a89e16da8ac0cd641
Sriram Karthik Akella <sriramka@ualberta.ca>
2024-08-05 22:36:34 +0300

    Update 5August2024 - 2053. Kth Distinct String in an Array.py

--- 5August2024 - 2053. Kth Distinct String in an Array.py
+++ 5August2024 - 2053. Kth Distinct String in an Array.py
@@ -10,3 +10,21 @@ class Solution:
         return ""
 
 # Time complexity: O(n^2) because we iterate through the arr list and count the number of occurrences of each element in the arr list.
+
+
+# Optimized solution:
+class Solution:
+    def kthDistinct(self, arr: List[str], k: int) -> str:
+        count = {}
+        for ele in arr:
+            if ele in count:
+                count[ele] += 1
+            else:
+                count[ele] = 1
+        
+        unique = [ele for ele in arr if count[ele] == 1]
+        
+        if len(unique) >= k:
+            return unique[k-1]
+        return ""
+
