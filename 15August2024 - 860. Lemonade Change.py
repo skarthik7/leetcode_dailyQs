f666626776bc6b63ba2022eb141f1e834a41e2c7
Sriram Karthik Akella <sriramka@ualberta.ca>
2024-08-15 10:03:56 +0300

    Update 15August2024 - 860. Lemonade Change.py

---
+++ 15August2024 - 860. Lemonade Change.py
@@ -0,0 +1,23 @@
+class Solution:
+    def lemonadeChange(self, bills: List[int]) -> bool:
+        currentFives = 0
+        currentTens = 0
+        currentTwenties = 0
+        for bill in bills:
+            if bill == 5:
+                currentFives+=1
+            elif bill == 10:
+                currentTens += 1
+                if currentFives == 0:
+                    return False
+                else:
+                    currentFives-=1
+            elif bill == 20:
+                if currentFives > 0 and currentTens > 0:
+                    currentFives -= 1
+                    currentTens -= 1
+                elif currentFives > 2 :
+                    currentFives -= 3
+                else:
+                    return False
+        return True
