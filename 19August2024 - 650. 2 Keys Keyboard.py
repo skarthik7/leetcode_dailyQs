71184ea51e0840c9b19beff0c62e36e00f7220ce
Sriram Karthik Akella <sriramka@ualberta.ca>
2024-08-19 07:53:28 +0300

    Update 19August2024 - 650. 2 Keys Keyboard.py

--- 19August2024 - 650. 2 Keys Keyboard.py
+++ 19August2024 - 650. 2 Keys Keyboard.py
@@ -1,3 +1,14 @@
 class Solution:
     def minSteps(self, n: int) -> int:
-        pass
+        if n == 1:
+            return 0
+        
+        dp = [0] * (n + 1)
+        
+        for i in range(2, n + 1):
+            dp[i] = i
+            for j in range(1, i // 2 + 1):
+                if i % j == 0:
+                    dp[i] = min(dp[i], dp[j] + (i // j))
+        
+        return dp[n]
