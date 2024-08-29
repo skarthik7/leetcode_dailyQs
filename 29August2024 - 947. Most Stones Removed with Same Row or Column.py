a5487200f45af5bbe3a849d6f99632ebd993a6cf
Sriram Karthik Akella <sriramka@ualberta.ca>
2024-08-29 23:57:44 +0530

    Update 29August2024 - 947. Most Stones Removed with Same Row or Column.py

--- 29August2024 - 947. Most Stones Removed with Same Row or Column.py
+++ 29August2024 - 947. Most Stones Removed with Same Row or Column.py
@@ -1,3 +1,24 @@
 class Solution:
     def removeStones(self, stones: List[List[int]]) -> int:
-        pass
+        parent = {}
+        
+        def find(x):
+            if parent[x] != x:
+                parent[x] = find(parent[x])
+            return parent[x]
+        
+        def union(x, y):
+            rootX = find(x)
+            rootY = find(y)
+            if rootX != rootY:
+                parent[rootX] = rootY
+        
+        for x, y in stones:
+            if (x, 'row') not in parent:
+                parent[(x, 'row')] = (x, 'row')
+            if (y, 'col') not in parent:
+                parent[(y, 'col')] = (y, 'col')
+            union((x, 'row'), (y, 'col'))
+        
+        unique_roots = len({find(x) for x in parent})
+        return len(stones) - unique_roots
