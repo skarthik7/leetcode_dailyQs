45f2bb032e612ca7c1fd2cba116e99c487672f5a
Sriram Karthik Akella <sriramka@ualberta.ca>
2024-09-13 18:46:46 -0600

    Update 13September2024 - 2419. Longest Subarray With Maximum Bitwise AND.py

--- 13September2024 - 2419. Longest Subarray With Maximum Bitwise AND.py
+++ 13September2024 - 2419. Longest Subarray With Maximum Bitwise AND.py
@@ -1,3 +1,14 @@
 class Solution:
     def longestSubarray(self, nums: List[int]) -> int:
-        pass
+        max_val = max(nums)
+        
+        longest = 0
+        current_length = 0
+        
+        for num in nums:
+            if num == max_val:
+                current_length += 1
+                longest = max(longest, current_length)
+            else:
+                current_length = 0
+        return longest
