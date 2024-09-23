dd1340599064a235d7b68809e2e51c48ee7a7ce2
Sriram Karthik Akella <sriramka@ualberta.ca>
2024-09-23 17:24:57 -0600

    Update 23September2024 - 2707. Extra Characters in a String.py

--- 23September2024 - 2707. Extra Characters in a String.py
+++ 23September2024 - 2707. Extra Characters in a String.py
@@ -1,3 +1,16 @@
 class Solution:
     def minExtraChar(self, s: str, dictionary: List[str]) -> int:
-        pass
+        n = len(s)
+        word_set = set(dictionary)
+        dp = [float('inf')] * (n + 1)
+        dp[0] = 0
+        
+        for i in range(1, n + 1):
+            for j in range(i):
+                if s[j:i] in word_set:
+                    dp[i] = min(dp[i], dp[j])
+                else:
+                    dp[i] = min(dp[i], dp[j] + (i - j))
+        
+        return dp[n]
+
