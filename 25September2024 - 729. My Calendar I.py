a7ca5f8f078b9f5b4b25ceec526bf9e64296365a
Sriram Karthik Akella <sriramka@ualberta.ca>
2024-09-25 21:26:08 -0600

    Update 25September2024 - 729. My Calendar I.py

---
+++ 25September2024 - 729. My Calendar I.py
@@ -0,0 +1,12 @@
+class MyCalendar:
+
+    def __init__(self):
+        pass
+
+    def book(self, start: int, end: int) -> bool:
+        
+        pass
+
+# Your MyCalendar object will be instantiated and called as such:
+# obj = MyCalendar()
+# param_1 = obj.book(start,end)
