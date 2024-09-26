1653c6a097cd82abab17fae4f907ad3035dcc220
Sriram Karthik Akella <sriramka@ualberta.ca>
2024-09-25 21:35:52 -0600

    Update 25September2024 - 729. My Calendar I.py

--- 25September2024 - 729. My Calendar I.py
+++ 25September2024 - 729. My Calendar I.py
@@ -1,11 +1,14 @@
 class MyCalendar:
 
     def __init__(self):
-        pass
+        self.bookings = []
 
     def book(self, start: int, end: int) -> bool:
-        
-        pass
+        for s, e in self.bookings:
+            if start < e and end > s:  # overlap
+                return False
+        self.bookings.append((start, end))
+        return True
 
 # Your MyCalendar object will be instantiated and called as such:
 # obj = MyCalendar()
