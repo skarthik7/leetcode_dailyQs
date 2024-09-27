5e392d110951facea486a9057b888a0c5062c7ab
Sriram Karthik Akella <sriramka@ualberta.ca>
2024-09-26 19:33:29 -0600

    Update 26September2024 - 731. My Calendar II.py

---
+++ 26September2024 - 731. My Calendar II.py
@@ -0,0 +1,12 @@
+class MyCalendarTwo:
+
+    def __init__(self):
+        pass
+        
+    def book(self, start: int, end: int) -> bool:
+        pass
+
+
+# Your MyCalendarTwo object will be instantiated and called as such:
+# obj = MyCalendarTwo()
+# param_1 = obj.book(start,end)
