534c80853561468477a6cf67ff08cb6e2fa168c6
Sriram Karthik Akella <sriramka@ualberta.ca>
2024-09-26 19:34:11 -0600

    Update 26September2024 - 731. My Calendar II.py

--- 26September2024 - 731. My Calendar II.py
+++ 26September2024 - 731. My Calendar II.py
@@ -1,10 +1,18 @@
 class MyCalendarTwo:
 
     def __init__(self):
-        pass
+        self.events = []
+        self.overlaps = []
         
     def book(self, start: int, end: int) -> bool:
-        pass
+        for o_start, o_end in self.overlaps:
+            if start < o_end and end > o_start:
+                return False
+        for e_start, e_end in self.events:
+            if start < e_end and end > e_start:
+                self.overlaps.append((max(start, e_start), min(end, e_end)))
+        self.events.append((start, end))
+        return True
 
 
 # Your MyCalendarTwo object will be instantiated and called as such:
