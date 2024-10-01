2b968c30451157afed1458062cf244ec2d985933
Sriram Karthik Akella <sriramka@ualberta.ca>
2024-10-01 13:50:26 -0600

    Update 1October2024 - 1497. Check If Array Pairs Are Divisible by k.py

---
+++ 1October2024 - 1497. Check If Array Pairs Are Divisible by k.py
@@ -0,0 +1,16 @@
+class Solution:
+    def canArrange(self, arr: List[int], k: int) -> bool:
+        remainder_count = [0] * k
+
+        for num in arr:
+            remainder = num % k
+            remainder_count[remainder] += 1
+
+        if remainder_count[0] % 2 != 0:
+            return False
+
+        for i in range(1, k // 2 + 1):
+            if remainder_count[i] != remainder_count[k - i]:
+                return False
+
+        return True
