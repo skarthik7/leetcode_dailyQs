28fb750d52cd4000ae5ec2fef909507b9e735f60
Sriram Karthik Akella <sriramka@ualberta.ca>
2024-10-01 21:12:22 -0600

    Update 2October2024 - 1331. Rank Transform of an Array.py

--- 2October2024 - 1331. Rank Transform of an Array.py
+++ 2October2024 - 1331. Rank Transform of an Array.py
@@ -1,3 +1,11 @@
 class Solution:
     def arrayRankTransform(self, arr: List[int]) -> List[int]:
-        pass
+        ranks = {}
+        sort_a = sorted(list(set(arr)))       
+        
+        for i in range(len(sort_a)):
+            ranks[sort_a[i]] = i+1
+          
+        for i in range(len(arr)):
+            arr[i] = ranks[arr[i]]
+        return arr
