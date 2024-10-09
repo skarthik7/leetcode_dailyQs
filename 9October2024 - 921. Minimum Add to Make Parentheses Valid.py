c5b73cca377f0b1c2b6a06b4689c829d59af924a
Sriram Karthik Akella <sriramka@ualberta.ca>
2024-10-08 22:41:12 -0600

    Update 9October2024 - 921. Minimum Add to Make Parentheses Valid.py

--- 9October2024 - 921. Minimum Add to Make Parentheses Valid.py
+++ 9October2024 - 921. Minimum Add to Make Parentheses Valid.py
@@ -1,3 +1,14 @@
 class Solution:
     def minAddToMakeValid(self, s: str) -> int:
-        pass
+        left_balance = 0
+        right_balance = 0
+        for char in s:
+            if char == '(':
+                left_balance += 1
+            elif char == ')':
+                if left_balance > 0:
+                    left_balance -= 1
+                else:
+                    right_balance += 1
+        
+        return left_balance + right_balance
