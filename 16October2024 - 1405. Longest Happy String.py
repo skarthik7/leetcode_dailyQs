ab0ad974a148bfa77316be806f9f5c1750eb9fb9
Sriram Karthik Akella <sriramka@ualberta.ca>
2024-10-16 01:28:10 -0600

    Update 16October2024 - 1405. Longest Happy String.py

--- 16October2024 - 1405. Longest Happy String.py
+++ 16October2024 - 1405. Longest Happy String.py
@@ -1,3 +1,30 @@
 class Solution:
     def longestDiverseString(self, a: int, b: int, c: int) -> str:
-        pass
+        max_heap = []
+        if a > 0:
+            heapq.heappush(max_heap, (-a, 'a'))
+        if b > 0:
+            heapq.heappush(max_heap, (-b, 'b'))
+        if c > 0:
+            heapq.heappush(max_heap, (-c, 'c'))
+        
+        result = []
+        
+        while max_heap:
+            count1, char1 = heapq.heappop(max_heap)
+            if len(result) >= 2 and result[-1] == result[-2] == char1:
+                if not max_heap:
+                    break
+                count2, char2 = heapq.heappop(max_heap)
+                result.append(char2)
+                count2 += 1
+                if count2 != 0:
+                    heapq.heappush(max_heap, (count2, char2))
+                heapq.heappush(max_heap, (count1, char1))
+            else:
+                result.append(char1)
+                count1 += 1
+                if count1 != 0:
+                    heapq.heappush(max_heap, (count1, char1))
+        
+        return ''.join(result)
