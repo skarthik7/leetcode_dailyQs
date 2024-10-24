ab21da3059c39c428e180beda68fc8763c70cb5e
Sriram Karthik Akella <sk.akella7@gmail.com>
2024-10-24 12:12:15 -0600

    Update 24October2024 - 951. Flip Equivalent Binary Trees.py

--- 24October2024 - 951. Flip Equivalent Binary Trees.py
+++ 24October2024 - 951. Flip Equivalent Binary Trees.py
@@ -6,4 +6,14 @@
 #         self.right = right
 class Solution:
     def flipEquiv(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
-        pass
+        if not root1 and not root2:
+            return True
+        if not root1 or not root2:
+            return False
+        if root1.val != root2.val:
+            return False
+        
+        without_flip = self.flipEquiv(root1.left, root2.left) and self.flipEquiv(root1.right, root2.right)
+        with_flip = self.flipEquiv(root1.left, root2.right) and self.flipEquiv(root1.right, root2.left)
+        
+        return without_flip or with_flip
