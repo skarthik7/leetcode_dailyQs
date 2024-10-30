1e3e3f6f9a8fa5d6a23a5cbfe4d66358c83f33a2
Sriram Karthik Akella <sk.akella7@gmail.com>
2024-10-30 12:13:54 -0600

    Update 29October2024 - 1671. Minimum Number of Removals to Make Mountain Array.py

--- 29October2024 - 1671. Minimum Number of Removals to Make Mountain Array.py
+++ 29October2024 - 1671. Minimum Number of Removals to Make Mountain Array.py
@@ -1,3 +1,22 @@
 class Solution:
     def minimumMountainRemovals(self, nums: List[int]) -> int:
-        pass
+        n = len(nums)
+        
+        lis = [1] * n
+        for i in range(n):
+            for j in range(i):
+                if nums[i] > nums[j]:
+                    lis[i] = max(lis[i], lis[j] + 1)
+        
+        lds = [1] * n
+        for i in range(n-1, -1, -1):
+            for j in range(i+1, n):
+                if nums[i] > nums[j]:
+                    lds[i] = max(lds[i], lds[j] + 1)
+        
+        max_mountain_len = 0
+        for i in range(1, n-1):
+            if lis[i] > 1 and lds[i] > 1:  # It must be a peak
+                max_mountain_len = max(max_mountain_len, lis[i] + lds[i] - 1)
+        
+        return n - max_mountain_len
