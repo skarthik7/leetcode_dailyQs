96dd8aee4ca5a925a590d8f40db48907134c3e6d
Sriram Karthik Akella <sk.akella7@gmail.com>
2024-10-31 10:41:41 -0600

    Update 30October2024 - 2463. Minimum Total Distance Traveled.py

--- 30October2024 - 2463. Minimum Total Distance Traveled.py
+++ 30October2024 - 2463. Minimum Total Distance Traveled.py
@@ -3,21 +3,21 @@ class Solution:
         robot.sort()
         factory.sort()
         
-        n = len(robot)
-        m = len(factory)
+        m, n = len(robot), len(factory)
         
-        #dp table        
-
-        dp = [[float('inf')] * (m + 1) for _ in range(n + 1)]
-        
+        dp = [[float('inf')] * (n + 1) for _ in range(m + 1)]
         dp[0][0] = 0
-        for i in range(1, n + 1):
-            for j in range(1, m + 1):
+
+        # dp table        
+        for j in range(1, n + 1):
+            dp[0][j] = 0
+            factory_position, factory_limit = factory[j - 1]
+            
+            for i in range(1, m + 1):
                 dp[i][j] = dp[i][j - 1]
                 
                 total_distance = 0
-                for k in range(1, min(i, factory[j - 1][1]) + 1):
-                    total_distance += abs(robot[i - k] - factory[j - 1][0])
+                for k in range(1, min(factory_limit, i) + 1):
+                    total_distance += abs(robot[i - k] - factory_position)
                     dp[i][j] = min(dp[i][j], dp[i - k][j - 1] + total_distance)
-        
-        return dp[n][m]
+        return dp[m][n]
