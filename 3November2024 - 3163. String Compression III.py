69467693493bad4b9ed61ab4c10023d23f488831
Sriram Karthik Akella <sk.akella7@gmail.com>
2024-11-04 11:09:28 -0700

    Update 3November2024 - 3163. String Compression III.py

--- 3November2024 - 3163. String Compression III.py
+++ 3November2024 - 3163. String Compression III.py
@@ -1,3 +1,9 @@
 class Solution:
     def compressedString(self, word: str) -> str:
-        pass
+        comp = ""
+        while word:
+            c = word[0]
+            prefix_length = min(9, len(word) - len(word.lstrip(c)))
+            comp += str(prefix_length) + c
+            word = word[prefix_length:]
+        return comp
