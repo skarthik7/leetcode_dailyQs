a8eda8c3ed7eb5b62982b0d9edf1c5d9ca6165c9
Sriram Karthik Akella <sriramka@ualberta.ca>
2024-11-09 15:21:17 -0700

    Update 9November2024 - 3133. Minimum Array End.py

--- 9November2024 - 3133. Minimum Array End.py
+++ 9November2024 - 3133. Minimum Array End.py
@@ -1,3 +1,13 @@
+# https://leetcode.com/problems/minimum-array-end/
 class Solution:
     def minEnd(self, n: int, x: int) -> int:
-        pass
+        nums = [x]
+        current = x
+        for i in range(1, n):
+            next_val = current + 1
+            while (next_val & x) != x:
+                next_val += 1
+            nums.append(next_val)
+            current = next_val
+        
+        return nums[-1]
