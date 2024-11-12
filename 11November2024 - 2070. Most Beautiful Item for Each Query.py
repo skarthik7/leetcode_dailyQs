1a7a37b6f72b2e032c9c4415dafd3a4635164223
Sriram Karthik Akella <sriramka@ualberta.ca>
2024-11-11 23:03:58 -0500

    Update 11November2024 - 2070. Most Beautiful Item for Each Query.py

--- 11November2024 - 2070. Most Beautiful Item for Each Query.py
+++ 11November2024 - 2070. Most Beautiful Item for Each Query.py
@@ -1,3 +1,4 @@
+# https://leetcode.com/problems/most-beautiful-item-for-each-query/
 class Solution:
     def maximumBeauty(self, items: List[List[int]], queries: List[int]) -> List[int]:
         pass
