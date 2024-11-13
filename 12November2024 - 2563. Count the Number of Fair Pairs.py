1adb78410b19a3dc7772cf43ab38036e08ebe7d4
Sriram Karthik Akella <sriramka@ualberta.ca>
2024-11-12 23:50:26 -0500

    Update 12November2024 - 2563. Count the Number of Fair Pairs.py

--- 12November2024 - 2563. Count the Number of Fair Pairs.py
+++ 12November2024 - 2563. Count the Number of Fair Pairs.py
@@ -1,3 +1,19 @@
 class Solution:
     def countFairPairs(self, nums: List[int], lower: int, upper: int) -> int:
-        pass
+        nums.sort() 
+        count = 0
+        n = len(nums)
+        
+        for i in range(n):
+            left = i + 1
+            while left < n and nums[i] + nums[left] < lower:
+                left += 1
+                
+            right = n - 1
+            while right > i and nums[i] + nums[right] > upper:
+                right -= 1
+                
+            if left <= right and right > i:
+                count += right - left + 1
+                
+        return count
