6942a51dd52ec3b43803dad47cf6c9956401f13a
Karthik <sk.akella7@gmail.com>
2024-11-22 11:07:11 -0700

    Update 22November2024 - Flip Columns For Maximum Number of Equal Rows.py

--- 22November2024 - Flip Columns For Maximum Number of Equal Rows.py
+++ 22November2024 - Flip Columns For Maximum Number of Equal Rows.py
@@ -1,4 +1,12 @@
 # https://leetcode.com/problems/flip-columns-for-maximum-number-of-equal-rows/
 class Solution:
     def maxEqualRowsAfterFlips(self, matrix: List[List[int]]) -> int:
-        pass
+        pattern_count = defaultdict(int)
+        
+        for row in matrix:
+            pattern = tuple(row)
+            flipped_pattern = tuple(1 - x for x in row)
+            pattern_count[pattern] += 1
+            pattern_count[flipped_pattern] += 1
+        
+        return max(pattern_count.values())
