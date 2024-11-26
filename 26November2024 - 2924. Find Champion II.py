34f1f6db31e3a56a797ca659de4342bdd291cedc
Karthik <sk.akella7@gmail.com>
2024-11-26 09:56:08 -0700

    Update 26November2024 - 2924. Find Champion II.py

--- 26November2024 - 2924. Find Champion II.py
+++ 26November2024 - 2924. Find Champion II.py
@@ -1,4 +1,15 @@
 #https://leetcode.com/problems/find-champion-ii/
 class Solution:
     def findChampion(self, n: int, edges: List[List[int]]) -> int:
-        pass
+        incoming = [0] * n
+        for _, v in edges:
+            incoming[v] += 1
+        
+        champion = -1
+        for i in range(n):
+            if incoming[i] == 0:
+                if champion != -1:
+                    return -1
+                champion = i
+        
+        return champion
