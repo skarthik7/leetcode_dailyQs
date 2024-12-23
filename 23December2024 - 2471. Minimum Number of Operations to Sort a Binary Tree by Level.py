da413b3403a149fdf2abab7542e15b6d91a3424b
Karthik <sk.akella7@gmail.com>
2024-12-23 11:31:20 -0700

    Update 23December2024 - 2471. Minimum Number of Operations to Sort a Binary Tree by Level.py

--- 23December2024 - 2471. Minimum Number of Operations to Sort a Binary Tree by Level.py
+++ 23December2024 - 2471. Minimum Number of Operations to Sort a Binary Tree by Level.py
@@ -6,4 +6,46 @@
 #         self.right = right
 class Solution:
     def minimumOperations(self, root: Optional[TreeNode]) -> int:
-        pass
+        if not root:
+            return 0
+        
+        def minSwaps(arr: List[int]) -> int:
+            n = len(arr)
+            arrpos = [(val, idx) for idx, val in enumerate(arr)]
+            arrpos.sort()
+            visited = [False] * n
+            swaps = 0
+            
+            for i in range(n):
+                if visited[i] or arrpos[i][1] == i:
+                    continue
+                
+                cycle_size = 0
+                j = i
+                while not visited[j]:
+                    visited[j] = True
+                    j = arrpos[j][1]
+                    cycle_size += 1
+                
+                if cycle_size > 0:
+                    swaps += (cycle_size - 1)
+            
+            return swaps
+        
+        queue = deque([root])
+        total_swaps = 0
+        
+        while queue:
+            level_size = len(queue)
+            level_values = []
+            
+            for _ in range(level_size):
+                node = queue.popleft()
+                level_values.append(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            total_swaps += minSwaps(level_values)
+        
+        return total_swaps
